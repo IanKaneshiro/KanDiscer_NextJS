435c3bc6270d951fb3a432f955b098190418ef4a
Ian <iandkaneshiro@gmail.com>
2024-03-24 18:49:52 -0700

    minor adjustments

--- src/app/bags/page.tsx
+++ src/app/bags/page.tsx
@@ -3,13 +3,11 @@ import style from "./bags.module.css";
 import Link from "next/link";
 
 const BagsNavigationBar = () => {
-  const [currentId, setCurrentId] = useState("");
-
   return (
     <div>
       <div className={style.navbar}>
         <button>Add a new bag</button>
-        <select value={currentId}>
+        <select>
           <option value="">Select your bag...</option>
         </select>
       </div>
