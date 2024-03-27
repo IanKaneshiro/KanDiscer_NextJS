707eec8b3e2ba1899e0a6e6dcd51d649332eeb7d
Ian <iandkaneshiro@gmail.com>
2024-03-26 18:51:47 -0700

    working with data

--- src/app/bags/[slug]/page.tsx
+++ src/app/bags/[slug]/page.tsx
@@ -1,3 +1,7 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+export default async function Page({ params }: { params: { slug: string } }) {
+  const data = await fetch(
+    `https://kandiscer.onrender.com/api/bags/${params.slug}`
+  ).then((res) => res.json());
+
+  return <div>My Post: {JSON.stringify(data)}</div>;
 }
